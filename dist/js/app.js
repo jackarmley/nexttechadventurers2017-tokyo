16d87c368bcdb741a75ec4b2b88bbdc5a2af419c
Jack Armley <jarmley@expedia.com>
2017-07-14 15:50:37 +0100

    Added dist and index files

---
+++ dist/js/app.js
@@ -0,0 +1,31 @@
+/**
+ * Shared js
+ */
+
+/**
+ * Site header js
+ */
+
+/**
+ * Site footer js
+ */
+
+/**
+ * Intro section js
+ */
+
+/**
+ * About section js
+ */
+
+/**
+ * Gallery section js
+ */
+
+/**
+ * Location section js
+ */
+
+/**
+ * Wishlist section js
+ */
